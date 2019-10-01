96df140dc15631b5c2d853170d726b484f61f6b9
Logan Troyer <troyerlogan@gmail.com>
2019-09-30 23:45:41 -0400

    Fixed errors

--- src/components/Screens/About.js
+++ src/components/Screens/About.js
@@ -1,57 +1,80 @@
-import React, { Component} from 'react'
-import ProfilePicture from '../Images/IMG_6919.jpeg'
-import Resume from '../Images/Resume.pdf'
-import Fade from 'react-reveal/Fade'
-import config from 'react-reveal/globals';
+import React, { Component } from "react";
+import ProfilePicture from "../Images/IMG_6919.jpeg";
+import Resume from "../Images/Resume.pdf";
+import Fade from "react-reveal/Fade";
+import config from "react-reveal/globals";
 
 class About extends Component {
-    render() {
-        let languages = [ ['ReactJs', 60], ['HTML', 70], ['Python', 50], ['JavaScript', 60] , ['Java', 40], ['CSS', 70] , ['VueJs', 50] ];
-        config({ ssrFadeout: true });
-        return (
-            <div className="about">
-                <div className="pt-5">
-                    <h4 className="display-4 text-center pt-2 titles">About</h4>
-                    <hr className="hr-sections"/>
-                </div>
-                <div className="wrapper flex-direction">
-                    <Fade left ssrFadeout>
-                        <div className="left">
-                            <img src = {ProfilePicture} className = "profilePic text-center" alt = "test"/>
-                            <h1>Who am I?</h1>
-                            <p>I am currently majoring in Computer Science with a minor in Mathematics </p>
-                            <p className="aboutText">at Indiana University-Purdue University Indianapolis (IUPUI).</p>
-                            <p className="aboutText">I am passionate about creating top of the line applications to fulfill</p>
-                            <p className="aboutText">customer needs!</p>
-                            <a href={Resume} target="_blank" rel = "noopener noreferrer"><button className="btn resumeButton">Resume</button></a>
-                        </div>
-                    </Fade>
-                    <div className="right">
-                        {
-                            languages.map((lang, idx) => {
-                                return (
-                                    <Fade left delay={(idx+2) +'00'}>
-                                        <div className="langs">
-                                            <div className="outerBar">
-                                                <div className="barTitle">
-                                                    <p>{lang[0]}</p>
-                                                </div>
-                                                <div className="innerBar">
-                                                        <div className="progressBar" style={{width: lang[1] + '%'}}>
-                                                            <p>{lang[1]}%</p>
-                                                        </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </Fade>
-                                )
-                            })
-                        }
-                    </div>
-                </div>
+  render() {
+    let languages = [
+      ["ReactJs", 60],
+      ["HTML", 70],
+      ["Python", 50],
+      ["JavaScript", 60],
+      ["Java", 40],
+      ["CSS", 70],
+      ["VueJs", 50]
+    ];
+    config({ ssrFadeout: true });
+    return (
+      <div className="about">
+        <div className="pt-5">
+          <h4 className="display-4 text-center pt-2 titles">About</h4>
+          <hr className="hr-sections" />
+        </div>
+        <div className="wrapper flex-direction">
+          <Fade left ssrFadeout>
+            <div className="left">
+              <img
+                src={ProfilePicture}
+                className="profilePic text-center"
+                alt="test"
+              />
+              <h1>Who am I?</h1>
+              <p>
+                I am currently majoring in Computer Science with a minor in
+                Mathematics{" "}
+              </p>
+              <p className="aboutText">
+                at Indiana University-Purdue University Indianapolis (IUPUI).
+              </p>
+              <p className="aboutText">
+                I am passionate about creating top of the line applications to
+                fulfill
+              </p>
+              <p className="aboutText">customer needs!</p>
+              <a href={Resume} target="_blank" rel="noopener noreferrer">
+                <button className="btn resumeButton">Resume</button>
+              </a>
             </div>
-        )
-    }
+          </Fade>
+          <div className="right">
+            {languages.map((lang, idx) => {
+              return (
+                <Fade left delay={idx + 2} key={idx}>
+                  <div className="langs">
+                    <div className="outerBar">
+                      <div className="barTitle">
+                        <p>{lang[0]}</p>
+                      </div>
+                      <div className="innerBar">
+                        <div
+                          className="progressBar"
+                          style={{ width: lang[1] + "%" }}
+                        >
+                          <p>{lang[1]}%</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </Fade>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default About
+export default About;
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,18 +1,14 @@
-import firebase from 'firebase'
+import firebase from "firebase";
 
 var config = {
-        apiKey: "AIzaSyA1pbddmS6kdFvq-xkUV2xktcynQ8nu3Vg",
-        authDomain: "website-2b805.firebaseapp.com",
-        databaseURL: "https://website-2b805.firebaseio.com",
-        projectId: "website-2b805",
-        storageBucket: "website-2b805.appspot.com",
-        messagingSenderId: "1086170405957",
-        appId: "1:1086170405957:web:8906fcc696b876b9"
-    };
-    firebase.initializeApp(config);
-    firebase.firestore().settings({ timestampsInSnapshots: true });
+  apiKey: "AIzaSyA1pbddmS6kdFvq-xkUV2xktcynQ8nu3Vg",
+  authDomain: "website-2b805.firebaseapp.com",
+  databaseURL: "https://website-2b805.firebaseio.com",
+  projectId: "website-2b805",
+  storageBucket: "website-2b805.appspot.com",
+  messagingSenderId: "1086170405957",
+  appId: "1:1086170405957:web:8906fcc696b876b9"
+};
+firebase.initializeApp(config);
 
-
-
-export default firebase
-
+export default firebase;
